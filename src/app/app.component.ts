84a9cab1206d84b57cf456d1a152d5ef23b1a1d0
Hidayat Bhatt <69143980+hidayatb2@users.noreply.github.com>
2024-11-03 18:29:00 +0530

    initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, HostListener } from '@angular/core';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +7,12 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'CaptureCrew';
+
+  isNavbarVisible = false;
+
+  @HostListener("window:scroll", [])
+  onWindowScroll() {
+    const headerHeight = document.querySelector('cc-header')?.clientHeight || 0;
+    this.isNavbarVisible = window.scrollY > headerHeight;
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { HeaderComponent } from './header/header.component';
+import { AboutUsComponent } from './about-us/about-us.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    HeaderComponent,
+    AboutUsComponent
   ],
   imports: [
     BrowserModule,
